b3ab0771573f3904bf5c113e582f07d79d318fa4
Nathan Lewan <nathanlewan@gmail.com>
2019-03-17 01:06:47 -0400

    playing

--- compare-hashtable/test.ps1
+++ compare-hashtable/test.ps1
@@ -1,39 +1,42 @@
+clear
+
 Describe 'compare-hashtable' {
-    Context 'simple-text output' {
+    Context 'test function: checkObject' {
 
-        it 'should return simple-text output' {
-            $initialObject = @{
+        it 'should return Hashtable' {
+            $Object = @{
                 username = "nathanl"
-                address = @{
-                    street = "crusade road"
-                    number = "515"
-                    people = @("me", "you", "us")
-                    things = @{
-                      thisThing = "this"
-                      thatThing = "that"
-                  }
-                }
-              }
+            }
               
-              $newObject = @{
-                username = "nlewan"
-                address = @{
-                    street = "crusade ave"
-                    number = "55"
-                    people = @("me", "you", "him")
-                }
-              }
 
-            $test = compare-hashtable -objA $initialObject -objB $newObject
+            $test = compare-nsObject -object $Object
 
-            $test.gettype().BaseType.Name | should be "Array"
+            $test | should be "Hashtable"
+            $test.length | should be 9
+
+        }
+
+        it 'should return Array' {
+            $Object = @(
+                "nathanl"
+            )
+              
+
+            $test = compare-nsObject -object $Object
+
+            $test | should be "Array"
+            $test.length | should be 5
+
+        }
+
+        it 'should return String' {
+            $Object = "nathanl"
+              
+
+            $test = compare-nsObject -object $Object
+
+            $test | should be "String"
             $test.length | should be 6
-            $test.contains("[-] , (key):address -> (arr):people , (value):us") | should be $true
-            $test.contains("[+] , (key):address -> (arr):people , (value):him") | should be $true
-            $test.contains("[-] , (key):address , (key):things") | should be $true
-            $test.contains("[*] , (key):address -> (key):number , (old_value):515  | (new_value):55") | should be $true
-            $test.contains("[*] , (key):address -> (key):street , (old_value):crusade road  | (new_value):crusade ave") | should be $true
-            $test.contains("[*] , (key):username , (old_value):nathanl | (new_value):nlewan") | should be $true
 
         }
     }
