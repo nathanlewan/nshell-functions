a8c920c980c87ac32a3bd8fac44555d8424b05fe
Nathan Lewan <nathanlewan@gmail.com>
2019-04-14 01:01:18 -0400

    playing

--- compare-hashtable/test.ps1
+++ compare-hashtable/test.ps1
@@ -1,43 +1,132 @@
 clear
 
-Describe 'compare-hashtable' {
-    Context 'test function: checkObject' {
+Describe 'compare-nsobject' {
+    Context 'test function: checkEntry - Hashtable detection' {
 
-        it 'should return Hashtable' {
+        it 'should return @(Hashtable, <empty>)' {
             $Object = @{
                 username = "nathanl"
             }
               
 
-            $test = compare-nsObject -object $Object
+            $test = compare-nsObject -objA $Object -runCheckEntry $true
 
-            $test | should be "Hashtable"
-            $test.length | should be 9
+            $test | should be @("OBJA: Hashtable", "OBJB: ")
+            $test.Length | should be 2
 
         }
 
-        it 'should return Array' {
-            $Object = @(
-                "nathanl"
-            )
+        it 'should return @(<empty>, Hashtable)' {
+            $Object = @{
+                username = "nathanl"
+            }
               
 
-            $test = compare-nsObject -object $Object
+            $test = compare-nsObject -objb $Object -runCheckEntry $true
 
-            $test | should be "Array"
-            $test.length | should be 5
+            $test | should be @("OBJA: ", "OBJB: Hashtable")
+            $test.Length | should be 2
 
         }
 
-        it 'should return String' {
+        it 'should return @(Hashtable, Hashtable)' {
+            $ObjectA = @{
+                username = "nathanl"
+            }
+            $ObjectB = @{
+                username = "nathanl"
+            }
+              
+
+            $test = compare-nsObject -objb $ObjectA -objA $ObjectB -runCheckEntry $true
+
+            $test | should be @("OBJA: Hashtable", "OBJB: Hashtable")
+            $test.Length | should be 2
+
+        }
+
+    }
+
+
+
+
+    Context 'test function: checkEntry - Array detection' {
+
+        it 'should return @(Array, <empty>)' {
+            $Object = @("nathanl")
+              
+
+            $test = compare-nsObject -objA $Object -runCheckEntry $true
+
+            $test | should be @("OBJA: Array", "OBJB: ")
+            $test.Length | should be 2
+
+        }
+
+        it 'should return @(<empty>, Array)' {
+            $Object = @("nathanl")
+              
+
+            $test = compare-nsObject -objb $Object -runCheckEntry $true
+
+            $test | should be @("OBJA: ", "OBJB: Array")
+            $test.Length | should be 2
+
+        }
+
+        it 'should return @(Array, Array)' {
+            $ObjectA = @("nathanl")
+            $ObjectB = @("nathanl")
+              
+
+            $test = compare-nsObject -objb $ObjectA -objA $ObjectB -runCheckEntry $true
+
+            $test | should be @("OBJA: Array", "OBJB: Array")
+            $test.Length | should be 2
+
+        }
+
+    }
+
+
+
+
+
+    Context 'test function: checkEntry - String detection' {
+
+        it 'should return @(String, <empty>)' {
             $Object = "nathanl"
               
 
-            $test = compare-nsObject -object $Object
+            $test = compare-nsObject -objA $Object -runCheckEntry $true
 
-            $test | should be "String"
-            $test.length | should be 6
+            $test | should be @("OBJA: String", "OBJB: ")
+            $test.Length | should be 2
 
         }
+
+        it 'should return @(<empty>, String)' {
+            $Object = "nathanl"
+              
+
+            $test = compare-nsObject -objb $Object -runCheckEntry $true
+
+            $test | should be @("OBJA: ", "OBJB: String")
+            $test.Length | should be 2
+
+        }
+
+        it 'should return @(String, String)' {
+            $ObjectA = "nathanl"
+            $ObjectB = "nathanl"
+              
+
+            $test = compare-nsObject -objb $ObjectA -objA $ObjectB -runCheckEntry $true
+
+            $test | should be @("OBJA: String", "OBJB: String")
+            $test.Length | should be 2
+
+        }
+
     }
 }
